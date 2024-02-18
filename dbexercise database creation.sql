95ce7ffd1630f599b1b7b50d707d5fec16425a08
ChaitnyaDwivedi <121715800+ChaitnyaDwivedi@users.noreply.github.com>
2024-02-18 13:25:59 +0530

    initial version
    database creation use database table creation and data insertion.

---
+++ dbexercise database creation.sql
@@ -0,0 +1,7 @@
+create database dbexercise;
+use dbexercise;
+create table student (name varchar(50),age int,rollno int);
+
+select * from dbexercise.student;
+insert into student (name,age,rollno) values
+("Chaitnya",20,101),
