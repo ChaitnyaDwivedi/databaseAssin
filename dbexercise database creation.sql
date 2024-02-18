587cbd88c42311ce7aee4b39096d8aad1ef8e5fc
ChaitnyaDwivedi <121715800+ChaitnyaDwivedi@users.noreply.github.com>
2024-02-18 13:35:22 +0530

    adding new row
    vishva record added

--- dbexercise database creation.sql
+++ dbexercise database creation.sql
@@ -4,4 +4,4 @@ create table student (name varchar(50),age int,rollno int);
 
 select * from dbexercise.student;
 insert into student (name,age,rollno) values
-("Chaitnya",20,101),
+("Chaitnya",20,101),("Vishva",30,102);
